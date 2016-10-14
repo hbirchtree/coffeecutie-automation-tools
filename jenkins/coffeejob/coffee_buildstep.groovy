133b6846f52fafb1a4030a5818091b8f896895f3
Håvard Bjerke <h0wy36@gmail.com>
2016-10-14 02:22:56 +0200

    - Fix up Jenkins DSL scripts, now creates seedjobs automatically (in theory)

--- jenkins/coffeejob/coffee_buildstep.groovy
+++ jenkins/coffeejob/coffee_buildstep.groovy
@@ -1,419 +1,261 @@
-def REPO_BRANCH = 'testing'
-def REPO_URL = 'https://github.com/hbirchtree/coffeecutie.git'
-
-def NUM_PLATFORMS = 4
-def NUM_FLAVORS = 2
-
-def CMAKE_GENERATORS = ['Ninja', 'Visual Studio 14 2015 Win64', 'Ninja', 'Ninja']
-def CMAKE_OPTIONS    = [
-    '',
-    '',
-    '',
-    '-DCOFFEE_BUILD_OPENAL=OFF'
-    ]
-def PLATFORM_STRING  = ['Linux', 'Windows', 'Android', 'OS X']
-def HAS_DEPENDENCIES = [false,   false,     true,      false]
-def PLATFORM_FLAG    = [0,       3,         1,         2]
-def PLATFORM_LABELS  = ['ubuntu && x64', 'windows && x64', 'android', 'macintosh && x64']
-def PLATFORM_PRELOAD = [
-    'x86_64-linux-generic.cmake',
-    'x86_64-windows-generic.cmake',
-    'armeabi_v7a-kitkat-linux-bionic.cmake',
-    'x86_64-osx-generic.cmake'
-    ]
-
-
 def RELEASE_TYPES = ['Debug', 'Release']
+def LINUX_PACKAGING_OPTS = "-DCOFFEE_GENERATE_APPIMAGE=ON -DCOFFEE_GENERATE_SNAPPY=ON -DCOFFEE_GENERATE_FLATPAK=ON"
+def Targets = [
+    /* Creates Ubuntu binaries linked against the system
+     * Also outputs AppImage, Snappy and Flatpak packages
+     * for better cross-compatibility.
+     */
+    new BuildTarget("Linux", "x86-64", "ubuntu && amd64",
+                   "x86_64-linux-generic.cmake",
+                   "native-linux-generic.toolchain.cmake",
+                   "Ninja", LINUX_PACKAGING_OPTS),
+    /* Creates Win32 applications, bog standard,
+     * self-contained resources.
+     */
+    new BuildTarget("Windows", "x86-64", "windows && x64",
+                   "x86_64-windows-generic.cmake",
+                   "x86_64-windows-win32.toolchain.cmake",
+                   "Visual Studio 14 2015", ""),
+    /* Windows UWP produces AppX directories for containment */
+    new BuildTarget("Windows UWP", "x86-64", "windows && x64",
+                   "x86_64-windows-uwp.cmake",
+                   "x86_64-windows-uwp.toolchain.cmake",
+                   "Visual Studio 14 2015", ""),
+    /* Good old OS X .app directories with some spice,
+     * self-contained resources.
+     */
+    new BuildTarget("Mac OS X", "x86-64", "macintosh && x64",
+                   "x86_64-osx-generic.cmake",
+                   "native-macintosh-generic.toolchain.cmake",
+                   "Ninja", "")
+]
 
-gitCheckoutProc = {
+class BuildTarget
+{
+    BuildTarget(String platName, String platArch,
+                String label, String cPreload,
+                String cTC, String cGen, String cOpts)
+    {
+        platformName = platName;
+        platformArch = platArch;
+        this.label = label;
+        cmake_preload = cPreload;
+        cmake_toolchain = cTC;
+        cmake_generator = cGen;
+        cmake_options = cOpts;
+    }
+
+    String platformName;
+    String platformArch;
+    String label;
+
+    String cmake_preload;
+    String cmake_toolchain;
+    String cmake_generator;
+    String cmake_options;
+};
+
+/* Setting up Git SCM
+ * One function to change them all
+ */
+void GetSourceStep(descriptor, job)
+{
+    def REPO_BRANCH = 'testing'
+        def REPO_URL = 'https://github.com/hbirchtree/coffeecutie.git'
+
+    job.with {
+        label(descriptor.label)
+        scm {
             git {
-              remote {
-                name('origin')
-                url("${REPO_URL}")
-              }
-              branch("${REPO_BRANCH}")
-              extensions {
-                relativeTargetDirectory('src')
-                submoduleOptions {
-                  recursive(true)
+                remote {
+                    name('origin')
+                    url(REPO_URL)
                 }
-                cloneOptions {
-                  shallow(true)
+                branch(REPO_BRANCH)
+                extensions {
+                    relativeTargetDirectory('src')
+                    submoduleOptions {
+                        recursive(true)
+                    }
+                    cloneOptions {
+                        shallow(true)
+                    }
                 }
-              }
             }
-          }
-gitTrigger = {
-            scm('H/10 * * * *')
-            githubPush()
-          }
-
-for(i in 0..(NUM_PLATFORMS-1)) {
-    def PLATFORM_NAME = PLATFORM_STRING[i]
-    def DEPENDS = HAS_DEPENDENCIES[i]
-    def FLAG = PLATFORM_FLAG[i]
-    def PLAT_LABEL = PLATFORM_LABELS[i]
-    def CMAKE_GENERATOR = CMAKE_GENERATORS[i]
-
-    def PIPELINE_NAME = "${PLATFORM_NAME}"
-    def WORKSPACE_LOC = "/tmp/${PLATFORM_NAME}/"
-
-    def GIT_CLONE = gitCheckoutProc
-    def GIT_TRIGGER = gitTrigger
-    
-    def PREV_STEP = ""
-    def DEPENDENCY_STEP = "0.0.${PLATFORM_NAME}-dep-setup"
-    
-    def DEP_STEPS = {}
-    def DEP_STATUS = 'SUCCESS'
-
-    deliveryPipelineView("${PIPELINE_NAME}") {
-      allowPipelineStart(true)
-      showTotalBuildTime(true)
-      pipelines {
-        component("${PLATFORM_NAME}",DEPENDENCY_STEP)
-      }
-    }
-    
-    if(FLAG == 2) {
-      DEP_STEPS = {
-        shell(
-          """
-            cd "${WORKSPACE_LOC}/src/desktop/osx/"
-            bash "gen_icons.sh"
-          """)
-      }
-      
-    job(DEPENDENCY_STEP) {
-      label("${PLAT_LABEL}")
-      customWorkspace("${WORKSPACE_LOC}")
-      deliveryPipelineConfiguration("${PIPELINE_NAME}","Dependency stage")
-      
-      steps {
-        shell(
-          """
-            cd "${WORKSPACE_LOC}/src/desktop/osx/"
-            bash "gen_icons.sh"
-          """)
-      }
-      
-      scm {
-      git {
-        remote {
-          name('origin')
-          url("${REPO_URL}")
         }
-        branch("${REPO_BRANCH}")
-        extensions {
-          relativeTargetDirectory('src')
-          submoduleOptions {
-            recursive(true)
-          }
-          cloneOptions {
-            shallow(true)
-          }
-        }
-      }
     }
+}
 
-      triggers {
-      scm('H/10 * * * *')
-      githubPush()
-    }
-    }
-    }else if(FLAG == 3) {
-      DEP_STATUS = 'FAILURE'
-      
-      job(DEPENDENCY_STEP) {
-      label("${PLAT_LABEL}")
-      customWorkspace("${WORKSPACE_LOC}")
-      deliveryPipelineConfiguration("${PIPELINE_NAME}","Dependency stage")
-      
-      steps {
-        batchFile(
-          """
-            mkdir build-Debug
-            mkdir build-Release
-            mklink /J build-Debug\\libs libs
-            mklink /J build-Release\\libs libs
-          """)
-      }
-      
-      scm {
-      git {
-        remote {
-          name('origin')
-          url("${REPO_URL}")
-        }
-        branch("${REPO_BRANCH}")
-        extensions {
-          relativeTargetDirectory('src')
-          submoduleOptions {
-            recursive(true)
-          }
-          cloneOptions {
-            shallow(true)
-          }
-        }
-      }
-    }
+void GetCMakeSteps(descriptor, job, variant, level)
+{
+    cmake_args = "-DCMAKE_TOOLCHAIN_FILE=${descriptor.cmake_toolchain} "
+    cmake_args += descriptor.cmake_options
 
-      triggers {
-      scm('H/10 * * * *')
-      githubPush()
-    }
-    }
-    }else {
-
-
-    job(DEPENDENCY_STEP) {
-      label("${PLAT_LABEL}")
-      customWorkspace("${WORKSPACE_LOC}")
-      deliveryPipelineConfiguration("${PIPELINE_NAME}","Dependency stage")
-      
-      scm {
-      git {
-        remote {
-          name('origin')
-          url("${REPO_URL}")
-        }
-        branch("${REPO_BRANCH}")
-        extensions {
-          relativeTargetDirectory('src')
-          submoduleOptions {
-            recursive(true)
-          }
-          cloneOptions {
-            shallow(true)
-          }
-        }
-      }
-    }
-
-      triggers {
-      scm('H/10 * * * *')
-      githubPush()
-    }
-    }
-    
-    }
-
-    PREV_STEP = DEPENDENCY_STEP
-    
-    if(DEPENDS) {
-        job("0.1.${PLATFORM_NAME}-dep-SDL2") {
-          label("${PLAT_LABEL}")
-          customWorkspace("${WORKSPACE_LOC}")
-
-          scm {
-            hg('https://hg.libsdl.org/SDL') {
-              tag('release-2.0.4')
-              subdirectory('SDL2')
-            }
-          }
-
-          triggers {
-            upstream(DEPENDENCY_STEP,DEP_STATUS)
-          }
-
-          steps {
+    job.with {
+        steps {
             cmake {
-              generator("${CMAKE_GENERATOR}")
-              args("-DCMAKE_INSTALL_PREFIX=${WORKSPACE_LOC}/libs -DSDL_SHARED=ON -DSDL_STATIC=ON -DSDL_STATIC_PIC=ON -DSDL_ATOMIC=OFF -DSDL_TIMERS=OFF -DSDL_AUDIO=OFF -DSDL_THREADS=OFF -DSDL_CPUINFO=OFF")
-              sourceDir('SDL2')
-              buildDir('SDL2_build')
-              buildType('Release')
-              buildToolStep {
-                useCmake(true)
-                args('--target install')
-              }
+                generator(descriptor.cmake_generator)
+                args(cmake_args)
+                preloadScript(descriptor.cmake_preload)
+                sourceDir('src')
+                buildDir("build_${variant}")
+                buildType(variant)
             }
-          }
         }
-
-        job("0.2.${PLATFORM_NAME}-dep-openal-soft") {
-          label("${PLAT_LABEL}")
-          customWorkspace("${WORKSPACE_LOC}")
-
-          scm {
-            git {
-              remote {
-                name('origin')
-                url("https://github.com/kcat/openal-soft.git")
-              }
-              branch("master")
-              extensions {
-                relativeTargetDirectory('openal-soft')
-                cloneOptions {
-                  shallow(true)
-                }
-              }
-            }
-          }
-
-          triggers {
-            upstream("0.1.${PLATFORM_NAME}-dep-SDL2",'SUCCESS')
-          }
-
-          steps {
-            cmake {
-              generator("${CMAKE_GENERATOR}")
-              args("-DCMAKE_INSTALL_PREFIX=${WORKSPACE_LOC}/libs")
-              sourceDir('openal-soft')
-              buildDir('openal-soft_build')
-              buildType('Release')
-              buildToolStep {
-                useCmake(true)
-                args('--target install')
-              }
-            }
-          }
-        }
-
-        job("0.9.${PLATFORM_NAME}-dep-complete") {
-          label("${PLAT_LABEL}")
-
-          triggers {
-            upstream("0.2.${PLATFORM_NAME}-dep-openal-soft",'SUCCESS')
-          }
-        }
-
-        PREV_STEP = "0.9.${PLATFORM_NAME}-dep-complete"
-        DEP_STEP = 'SUCCESS'
-    }
-
-    for(j in 0..(NUM_FLAVORS-1)) {
-        def JOB_NUM = j + 1
-        def JOB_RELEASE = RELEASE_TYPES[j]
-
-        cmakeTypicalBuild = {
-                    cmake {
-                      generator("${CMAKE_GENERATOR}")
-                      args('-DCMAKE_INSTALL_PREFIX=out')
-                      sourceDir('src')
-                      buildDir("build-${JOB_RELEASE}")
-                      buildType("${JOB_RELEASE}")
-                      buildToolStep {
-                        useCmake(true)
-                            args('--target install')
-                      }
-                    }
-                  }
-        cmakeTypicalTest = {
-                    cmake {
-                      generator("${CMAKE_GENERATOR}")
-                      args('-DCMAKE_INSTALL_PREFIX=out')
-                      sourceDir('src')
-                      buildDir("build-${JOB_RELEASE}")
-                      buildType("${JOB_RELEASE}")
-                      buildToolStep {
-                        useCmake(true)
-                        args('--target test')
-                      }
-                    }
-                  }
-        
-        def BUILDSTEPS = cmakeTypicalBuild
-        def TESTSTEPS = cmakeTypicalTest
-        
-        def COMPILE_STEP = "${JOB_NUM}.0.${PLATFORM_NAME}-${JOB_RELEASE}-compile"
-        def TEST_STEP = "${JOB_NUM}.1.${PLATFORM_NAME}-${JOB_RELEASE}-test"
-
-        job(COMPILE_STEP) {
-          label("${PLAT_LABEL}")
-          customWorkspace("${WORKSPACE_LOC}")
-          deliveryPipelineConfiguration("${PIPELINE_NAME}","${JOB_RELEASE} build")
-
-          triggers {
-            upstream(PREV_STEP,DEP_STATUS)
-          }
-
-          steps {
-                    cmake {
-                      generator("${CMAKE_GENERATOR}")
-                      args('-DCMAKE_INSTALL_PREFIX=out')
-                      sourceDir('src')
-                      buildDir("build-${JOB_RELEASE}")
-                      buildType("${JOB_RELEASE}")
-                      buildToolStep {
-                        useCmake(true)
-                            args('--target install')
-                      }
-                    }
-                  }
-
-          properties {
+        /* Fixes some issues on Mac OS X with finding Brew */
+        properties {
             environmentVariables {
-              keepSystemVariables(true)
-              keepBuildVariables(true)
+                keepSystemVariables(true)
+                keepBuildVariables(true)
             }
-          }
         }
+    }
 
-        if(FLAG != 3){
-            job(TEST_STEP) {
-              label("${PLAT_LABEL}")
-              customWorkspace("${WORKSPACE_LOC}")
-              deliveryPipelineConfiguration("${PIPELINE_NAME}","${JOB_RELEASE} test")
-
-              triggers {
-                upstream(COMPILE_STEP,'SUCCESS')
-              }
-
-              steps {
-                        cmake {
-                          generator("${CMAKE_GENERATOR}")
-                          args('-DCMAKE_INSTALL_PREFIX=out')
-                          sourceDir('src')
-                          buildDir("build-${JOB_RELEASE}")
-                          buildType("${JOB_RELEASE}")
-                          buildToolStep {
-                            useCmake(true)
-                            args('--target test')
-                          }
-                        }
-                      }
-
-              publishers {
-                textFinder('The following tests FAILED','',true,false,true)
-                archiveArtifacts {
-                  pattern("build-${JOB_RELEASE}/out/**")
+    if(level == 0)
+    {
+        job.with {
+            steps {
+                cmake {
+                    buildToolStep {
+                        useCmake(true)
+                        args("--target install")
+                    }
                 }
-              }
+            }
+        }
+    }else if(level == 1)
+    {
+        if(descriptor.platformName != "Windows"
+           && descriptor.platformName != "Windows UWP")
+        {
+            /* Run CTest */
+            job.with {
+                steps {
+                    cmake {
+                        buildToolStep {
+                            useCmake(true)
+                            args("--target test")
+                        }
+                    }
+                }
             }
         }else{
-            job(TEST_STEP) {
-              label("${PLAT_LABEL}")
-              customWorkspace("${WORKSPACE_LOC}")
-              deliveryPipelineConfiguration("${PIPELINE_NAME}","${JOB_RELEASE} test")
-
-              triggers {
-                upstream(COMPILE_STEP,'SUCCESS')
-              }
-
-              steps {
-                        cmake {
-                          generator("${CMAKE_GENERATOR}")
-                          args('-DCMAKE_INSTALL_PREFIX=out')
-                          sourceDir('src')
-                          buildDir("build-${JOB_RELEASE}")
-                          buildType("${JOB_RELEASE}")
-                          buildToolStep {
+            /* Visual Studio names the tests differently */
+            job.with {
+                /* Run tests */
+                steps {
+                    cmake {
+                        buildToolStep {
                             useCmake(true)
-                            args('--target RUN_TESTS')
-                          }
+                            args("--target RUN_TESTS")
                         }
-                      }
-
-              publishers {
-                textFinder('The following tests FAILED','',true,false,true)
-                archiveArtifacts {
-                  pattern("build-${JOB_RELEASE}/out/**")
+                    }
                 }
-              }
             }
         }
 
-        PREV_STEP = TEST_STEP
+        /* Add artifact step, archiving the binaries */
+        job.with {
+            publishers {
+                textFinder("The following tests FAILED",'', true, false, true)
+                archiveArtifacts {
+                    pattern("build_${variant}/out/**")
+                }
+            }
+        }
+    }
+}
+
+/* Adds platform-specific features to jobs
+ * Useful for fixing issues
+ */
+void GetJobQuirks(descriptor, compile, testing, workspace)
+{
+    if(descriptor.platformName == "Windows"
+       || descriptor.platformName == "Windows UWP")
+    {
+        /* Linking library directories */
+        compile.with {
+            steps {
+                batchFile(
+                  """
+                    mkdir build_Debug
+                    mkdir build_Release
+                    mklink /J build_Debug\\libs libs
+                    mklink /J build_Release\\libs libs
+                  """)
+            }
+        }
+    }else if(descriptor.platformName == "Mac OS X")
+    {
+        compile.with {
+            steps {
+                shell(
+                  """
+                                        cd "${workspace}/src/desktop/osx/"
+                    bash "gen_icons.sh"
+                  """)
+            }
+        }
+    }
+}
+
+for(t in Targets) {
+    pipelineName = "${t.platformName}_${t.platformArch}"
+    workspaceDir = "/tmp/${pipelineName}"
+
+    /* Create a pipeline per build target */
+    pip = deliveryPipelineView("${pipelineName}")
+
+    /* Acquiring the source code is step 0 */
+    source_step = job("0.0_${pipelineName}_Setup")
+    /* One function to insert the SCM data */
+    GetSourceStep(t, source_step)
+
+    source_step.with {
+        customWorkspace(workspaceDir)
+    }
+
+    pip.with {
+        allowPipelineStart(true)
+        showTotalBuildTime(true)
+        pipelines {
+            component(pipelineName, source_step.name)
+        }
+    }
+
+    last_step = source_step.name
+    i = 1
+
+    for(rel in RELEASE_TYPES)
+    {
+        compile = job("${i}.0_${pipelineName}_${rel}")
+        testing = job("${i}.1_${pipelineName}_${rel}_Testing")
+
+        i++;
+
+        /* Compilation and testing will only be performed on suitable hosts */
+        compile.with {
+            label(t.label)
+            customWorkspace(workspaceDir)
+            deliveryPipelineConfiguration(pipelineName, "${rel} compilation stage")
+            triggers {
+                upstream(last_step)
+            }
+        }
+        testing.with {
+            label(t.label)
+            customWorkspace(workspaceDir)
+            deliveryPipelineConfiguration(pipelineName, "${rel} testing stage")
+            triggers {
+                upstream(compile.name)
+            }
+        }
+
+        last_step = testing.name
+
+        GetJobQuirks(t, compile, testing, workspaceDir)
+        GetCMakeSteps(t, compile, rel, 0)
+        GetCMakeSteps(t, testing, rel, 1)
     }
 }
